b91d42eb3342793073040b9ddfe5821023ea79b8
Ortega1702 <131114108+Ortega1702@users.noreply.github.com>
2023-11-03 14:19:38 -0500

    Add files via upload

---
+++ proyecto_sena (1).sql
@@ -0,0 +1,199 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1:3308
+-- Tiempo de generación: 03-11-2023 a las 20:18:49
+-- Versión del servidor: 10.4.28-MariaDB
+-- Versión de PHP: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `proyecto_sena`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `dispositivos`
+--
+
+CREATE TABLE `dispositivos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `hardware` varchar(50) NOT NULL,
+  `id_unidad` int(11) NOT NULL,
+  `descripcion` varchar(200) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `dispositivos`
+--
+
+INSERT INTO `dispositivos` (`id`, `nombre`, `hardware`, `id_unidad`, `descripcion`, `id_usuario`) VALUES
+(2, 'Temperatura', 'ESP32', 1, 'Temperatura de peces', 1),
+(3, 'LED', 'ESP32', 2, 'Led de invernaderos', 1),
+(4, 'Turbiedad', 'ESP32', 1, 'Turbiedad del agua ', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `temperaturas`
+--
+
+CREATE TABLE `temperaturas` (
+  `id` int(11) NOT NULL,
+  `fecha` date DEFAULT NULL,
+  `temperatura` decimal(5,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `temperaturas`
+--
+
+INSERT INTO `temperaturas` (`id`, `fecha`, `temperatura`) VALUES
+(1, '2023-10-01', 25.50),
+(2, '2023-10-02', 26.00),
+(3, '2023-10-03', 24.80),
+(4, '2023-10-04', 23.70),
+(5, '2023-10-05', 22.30),
+(28, '2023-10-11', 22.50),
+(29, '2023-10-12', 23.00),
+(30, '2023-10-13', 21.80);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `unidades`
+--
+
+CREATE TABLE `unidades` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `id_usuario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `unidades`
+--
+
+INSERT INTO `unidades` (`id`, `nombre`, `id_usuario`) VALUES
+(1, 'Peces', 1),
+(2, 'Invernaderos', 1),
+(4, 'Apicultura', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `contrasena` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `apellido`, `usuario`, `email`, `contrasena`) VALUES
+(1, 'Mateo', 'Restrepo Molina', 'Mateo255', 'mateorestrepo35@gmail.com', '12345'),
+(3, 'Julián David', 'Ortega Sanmartín', 'Ortega2023', 'julianortega985@gmail.com', '12345'),
+(4, 'Carlos ', 'Bernal', 'CarloB.', 'carlosandresbernalsaldarriaga@gmail.com', '03032004');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_unidad` (`id_unidad`),
+  ADD KEY `dispositivos_ibfk_2` (`id_usuario`);
+
+--
+-- Indices de la tabla `temperaturas`
+--
+ALTER TABLE `temperaturas`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_usuario` (`id_usuario`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `temperaturas`
+--
+ALTER TABLE `temperaturas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+
+--
+-- AUTO_INCREMENT de la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `dispositivos`
+--
+ALTER TABLE `dispositivos`
+  ADD CONSTRAINT `dispositivos_ibfk_1` FOREIGN KEY (`id_unidad`) REFERENCES `unidades` (`id`),
+  ADD CONSTRAINT `dispositivos_ibfk_2` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+
+--
+-- Filtros para la tabla `unidades`
+--
+ALTER TABLE `unidades`
+  ADD CONSTRAINT `unidades_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
